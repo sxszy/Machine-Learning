9f1986261cd2954ad543eb5d20a57aa99d7da8b4
Manu <qzszy@foxmail.com>
2018-05-22 17:01:07 +0800

    Create Association Rule.py

---
+++ Homework/Association Rule.py
@@ -0,0 +1,10 @@
+#import
+import Orange
+
+## Read the file
+data = Orange.data.Table("mammographic_masses.csv")
+rules = Orange.associate.AssociationRulesSparseInducer(data, support=0.01, confidence=0.01)
+print ("%4s %4s  %s" , "Supp", "Conf", "Rule")
+for r in rules:
+    print ("%.2f %.2f  %s", r.support, r.confidence, r)
+    
